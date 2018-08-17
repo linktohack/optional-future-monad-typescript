a6c69ecfb52c831fde84cfa835a8fe2436795546
Quang-Linh LE <linktohack@gmail.com>
2018-08-17 09:48:42 +0700

    Modernize things

--- index.js
+++ index.js
@@ -1,44 +1,70 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var lib_1 = require("./lib");
+const lib_1 = require("./lib");
 lib_1.Optional
     .of({ x: 5, y: { z: { t: 10 } } })
     .k('y')
     .k('z')
     .k('t')
-    .flatMap(function (it) { return new lib_1.Optional(JSON.stringify(it) + " aaaa"); })
+    .flatMap(it => new lib_1.Optional(`${JSON.stringify(it)} aaaa`))
     .match({
-    some: function (x) {
+    some(x) {
         console.log('this is x', x);
     },
-    none: function () {
+    none() {
         console.log('nothing');
     }
 });
 console.log('val', lib_1.Optional
     .of(1)
-    .flatMap(function (it) { return lib_1.Optional
+    .flatMap(it => lib_1.Optional
     .of(2)
-    .map(function (it2) { return it + it2; }); })
+    .map(it2 => it + it2))
     .value);
+const optional = lib_1.doM(function* () {
+    const val1 = yield lib_1.Optional.of(1);
+    const val2 = yield lib_1.Optional.of(2);
+    return lib_1.Optional.of(val1 + val2);
+});
+optional.match({
+    some(it) {
+        console.log('some', it);
+    },
+    none() {
+        console.log('none');
+    }
+});
+const future = lib_1.doM(function* () {
+    const val1 = yield new lib_1.Future(((resolve, reject) => setTimeout(() => resolve(5), 1000)));
+    console.log('val1', val1);
+    const val2 = yield new lib_1.Future(((resolve, reject) => setTimeout(() => resolve(7), 2000)));
+    console.log('val2', val2);
+    return new lib_1.Future(((resolve, reject) => setTimeout(() => resolve(val1 + val2), 3000)));
+});
+future.match({
+    resolve(x) {
+        console.log(x);
+    },
+    reject: console.log.bind(console)
+});
 lib_1.Future
     .resolve(5)
-    .map(function (it) {
+    .map(it => {
     return it + 1;
 })
-    .flatMap(function (it) {
-    return new lib_1.Future(function (resolve, reject) {
-        setTimeout(function () {
+    .flatMap(it => {
+    return new lib_1.Future((resolve, reject) => {
+        setTimeout(() => {
             // resolve(`${it} bbb`);
             reject(new Error('what'));
         }, 1000);
     });
 })
     .match({
-    resolve: function (value) {
+    resolve(value) {
         console.log('resolve', value);
     },
-    reject: function (error) {
+    reject(error) {
         console.log('reject', error);
     }
 });
--- lib.js
+++ lib.js
@@ -1,87 +1,91 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Optional = /** @class */ (function () {
-    function Optional(value) {
+function doM(generator) {
+    const gen1 = generator();
+    return function fn(value) {
+        const result = gen1.next(value);
+        if (result.done) {
+            return result.value;
+        }
+        return result.value.flatMap(fn);
+    }();
+}
+exports.doM = doM;
+class Optional {
+    constructor(value) {
         this.value = value;
     }
-    Optional.of = function (value) {
+    static of(value) {
         return new Optional(value);
-    };
-    Optional.prototype.flatMap = function (fn) {
+    }
+    flatMap(fn) {
         if (!this.value) {
             return new Optional(this.value);
         }
         return fn(this.value);
-    };
-    Optional.prototype.map = function (fn) {
+    }
+    map(fn) {
         if (!this.value) {
             return new Optional(this.value);
         }
         return new Optional(fn(this.value));
-    };
-    Optional.prototype.k = function (key) {
-        return this.map(function (value) { return value[key]; });
-    };
-    Optional.prototype.match = function (_a) {
-        var some = _a.some, none = _a.none;
+    }
+    k(key) {
+        return this.map(value => value[key]);
+    }
+    match({ some, none }) {
         if (!this.value && none) {
             return none();
         }
         if (this.value && some) {
             return some(this.value);
         }
-    };
-    return Optional;
-}());
-exports.Optional = Optional;
-var Future = /** @class */ (function () {
-    function Future(fn) {
-        var _this = this;
-        this.fn = fn;
-        this.resolve = function (_) {
-        };
-        this.reject = function (_) {
-        };
-        fn(function (value) { return _this.resolve(value); }, function (error) { return _this.reject(error); });
     }
-    Future.resolve = function (value) {
-        return new Future(function (resolve, _) {
-            setImmediate(function () {
+}
+exports.Optional = Optional;
+class Future {
+    constructor(fn) {
+        this.fn = fn;
+        this.resolve = (_) => {
+        };
+        this.reject = (_) => {
+        };
+        fn((value) => this.resolve(value), error => this.reject(error));
+    }
+    static resolve(value) {
+        return new Future((resolve, _) => {
+            setImmediate(() => {
                 resolve(value);
             });
         });
-    };
-    Future.reject = function (error) {
-        return new Future(function (_, reject) {
-            setImmediate(function () {
+    }
+    static reject(error) {
+        return new Future((_, reject) => {
+            setImmediate(() => {
                 reject(error);
             });
         });
-    };
-    Future.prototype.flatMap = function (fn) {
-        var _this = this;
-        return new Future(function (resolve, reject) {
-            _this.resolve = function (value) {
+    }
+    flatMap(fn) {
+        return new Future((resolve, reject) => {
+            this.resolve = (value) => {
                 fn(value)
-                    .match({ resolve: resolve, reject: reject });
+                    .match({ resolve, reject });
             };
-            _this.reject = reject;
+            this.reject = reject;
         });
-    };
-    Future.prototype.map = function (fn) {
-        var _this = this;
-        return new Future(function (resolve, reject) {
-            _this.resolve = function (value) {
+    }
+    map(fn) {
+        return new Future((resolve, reject) => {
+            this.resolve = (value) => {
                 resolve(fn(value));
             };
-            _this.reject = reject;
+            this.reject = reject;
         });
-    };
-    Future.prototype.match = function (_a) {
-        var resolve = _a.resolve, reject = _a.reject;
+    }
+    match({ resolve, reject }) {
         this.resolve = resolve;
         this.reject = reject;
-    };
-    return Future;
-}());
+    }
+}
 exports.Future = Future;
